de95cd3aa3cc2ffb9f1fc5bba8759bce0d629c79
Ronil Maharjan <134940886+akeraaaa@users.noreply.github.com>
2026-07-02 23:32:36 +0545

    Update main.dart

--- fittrackerddemo/main.dart
+++ fittrackerddemo/main.dart
@@ -137,15 +137,7 @@ class _HomeScreenState extends State<HomeScreen>
   }
   
 
-  @override
-  
-  void dispose() {
-    
-    _ringCtrl.dispose();
-    
-    super.dispose();
-    
-  }
+ 
   
 
   @override
