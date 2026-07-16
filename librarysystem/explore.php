1c68ea8e1146f9169bb17402d55adb1f6691c35a
Ronil Maharjan <134940886+akeraaaa@users.noreply.github.com>
2026-07-16 23:57:07 +0545

    Enhance explore.php with contact and footer sections
    Added a contact section and footer to the explore page.

--- librarysystem/explore.php
+++ librarysystem/explore.php
@@ -133,5 +133,23 @@
       </div>
       <!-- End of book section -->
      
+ <!-- Contact Us section -->
+      <div id="contact">
+        <?php 
+         include ('contactUs.php');
+         ?>
+      </div>
+      <!-- End of contact us section -->
+
+      <!-- Footer section -->
+      <div class="footer">
+        <?php
+       include('footer.html');
+       ?></div>
+      <!-- End of footer section -->
+    </div>
+    <script src="js/explore.js"></script>
+  </body>
+</html>
 
      
